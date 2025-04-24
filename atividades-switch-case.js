32bc99560146b7fc0075e4a0ddc73d4f004cdb7b
arthur cavalcante <linsarthur02@gmail.com>
2025-04-24 16:46:19 -0300

    Aula 24/04

---
+++ atividades-switch-case.js
@@ -0,0 +1,162 @@
+// let pontoCardeal = "norte";
+
+// switch (pontoCardeal) {
+//     case "norte":
+//         console.log("Pra cima");
+//         break;
+
+//     case "sul":
+//         console.log("Pra baixo");
+//         break;
+
+
+//     case "leste":
+//         console.log("Pra direita");
+//         break;
+
+
+//     case "oeste":
+//         console.log("Pra esquerda");
+//         break;
+
+//     default: 
+//     console.log("Informe uma opção válida");
+
+// }
+//-----------------------------------------------------
+
+// let dia = 1
+
+// switch (dia) {
+//     case 1:
+//         console.log("Domingo");
+//         break;
+//     case 2:
+//         console.log("Segunda-feira");
+//         break;
+//     case 3:
+//         console.log("Terça-feira");
+//         break;
+//     case 4:
+//         console.log("Quarta-feira");
+//         break;
+//     case 5:
+//         console.log("Quinta-feira");
+//         break;
+//     case 6:
+//         console.log("Sexta-feira");
+//         break;
+//     case 7:
+//         console.log("Sábado");
+//         break;
+//     default:
+//         console.log("Não entendi o dia");
+
+// }
+
+//---------------------------------------------
+
+// let nota = "F"
+// switch (nota) {
+//     case "A":
+//         console.log("Ótimo");
+//         break;
+//     case "B":
+//         console.log("Bom");
+//         break;
+//     case "C":
+//         console.log("Regular");
+//         break;
+//     case "D":
+//         console.log("Ruim");
+//         break;
+//     case "F":
+//         console.log("Reprovado");
+//         break;
+
+//     default:
+//         console.log("Nota desconhecida");
+
+// }
+
+//----------------------------------------------------
+
+// let opcao = 4
+// let numero1 = 8;
+// let numero2 = 0;
+// let operacao;
+
+// switch (opcao) {
+//     case 1:
+//         operacao = numero1 + numero2
+//         console.log(operacao.toFixed(2));
+//         break;
+//     case 2:
+//         operacao = numero1 - numero2
+//         console.log(operacao.toFixed(2));
+//         break;
+//     case 3:
+//         operacao = numero1 * numero2
+//         console.log(operacao.toFixed(2));
+//         break;
+//     case 4:
+//         if(numero2 != 0){
+//             operacao = numero1 / numero2
+//             console.log(operacao.toFixed(2));
+//         }else{
+//             console.log("Não existe divisão por zero");
+            
+//         }
+//         break;
+
+//         default:
+//             console.log("Operação não encontrada");
+
+// }
+
+//-------------------------------------------------------
+
+// let mes = 12
+
+// switch (mes) {
+//     case 1:
+//         console.log("Janeiro");
+//         break;
+//     case 2:
+//         console.log("Fevereiro");
+//         break;
+//     case 3:
+//         console.log("Março");
+//         break;
+//     case 4:
+//         console.log("Abril");
+//         break;
+//     case 5:
+//         console.log("Maio");
+//         break;
+//     case 6:
+//         console.log("Junho");
+//         break;
+//     case 7:
+//         console.log("Julho");
+//         break;
+//     case 8:
+//         console.log("Agosto");
+//         break;
+//     case 9:
+//         console.log("Setembro");
+//         break;
+//     case 10:
+//         console.log("Outubro");
+//         break;
+//     case 11:
+//         console.log("Novembro");
+//         break;
+//     case 12:
+//         console.log("Dezembro");
+//         break;
+
+//         default:
+//             console.log("Mês não entendido.");
+            
+// }
