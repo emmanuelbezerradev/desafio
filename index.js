cfc3e7e29860bc1c0daa038ec1dc4216afcc1a0a
Emmanuel Bezerra <emmanuelbezerra1992@gmail.com>
2025-04-26 20:28:15 -0300

    26/04 tela de cadastro

--- index.js
+++ index.js
@@ -1,145 +1,175 @@
-function descobrirNome() {
-    let diaE = document.querySelector('#dia').value;
-    let mesE = document.querySelector('#mes').value;
+// function descobrirNome() {
+//     let diaE = document.querySelector('#dia').value;
+//     let mesE = document.querySelector('#mes').value;
 
-    let mesEscolhido = "";
-    let diaEscolhido = "";
+//     let mesEscolhido = "";
+//     let diaEscolhido = "";
 
-    switch (mesE) {
-        case "1":
-            mesEscolhido = "Sabugo"
+//     switch (mesE) {
+//         case "1":
+//             mesEscolhido = "Sabugo"
+//             break;
+//         case "2":
+//             mesEscolhido = "Seborreio"
+//             break;
+//         case "3":
+//             mesEscolhido = "Sirico"
+//             break;
+//         case "4":
+//             mesEscolhido = "Xoxoto"
+//         case "5":
+//             mesEscolhido = "Menstruado"
+//             break;
+//         case "6":
+//             mesEscolhido = "Delicio"
+//             break;
+//         case "7":
+//             mesEscolhido = "Caga tronco"
+//             break;
+//         case "8":
+//             mesEscolhido = "Mestruado"
+//             break;
+//         case "9":
+//             mesEscolhido = "Pertubados"
+//             break;
+//         case "10":
+//             mesEscolhido = "Hemorroido"
+//             break;
+//         case "11":
+//             mesEscolhido = "Testosterono"
+//             break;
+//         case "12":
+//             mesEscolhido = "Lapiseiro"
+//             break;
+//         default:
+//             mesEscolhido;
+//     }
+
+
+//     switch (diaE) {
+//         case "1":
+//             diaEscolhido = "do Tuti fruto"
+//             break;
+//         case "2":
+//             diaEscolhido = "do Tadalafilo"
+//             break;
+//         case "3":
+//             diaEscolhido = "do Prexeco"
+//             break;
+//         case "4":
+//             diaEscolhido = "do Tilápio"
+//             break;
+//         case "5":
+//             diaEscolhido = "do Camisinho"
+//             break;
+//         case "6":
+//             diaEscolhido = "do Macaxeiro"
+//             break;
+//         case "7":
+//             diaEscolhido = "do Jiboio"
+//             break;
+//         case "8":
+//             diaEscolhido = "do Cenouro"
+//             break;
+//         case "9":
+//             diaEscolhido = "do Surubo"
+//             break;
+//         case "10":
+//             diaEscolhido = "do Lagartixo"
+//             break;
+//         case "11":
+//             diaEscolhido = "do Toscano"
+//             break;
+//         case "12":
+//             diaEscolhido = "dO Janjolão"
+//             break;
+//         case "13":
+//             diaEscolhido = "do Pitango"
+//             break;
+//         case "14":
+//             diaEscolhido = "do Vidoco"
+//             break;
+//         case "15":
+//             diaEscolhido = "do Danono"
+//             break;
+//         case "16":
+//             diaEscolhido = "do Bisteco"
+//             break;
+//         case "17":
+//             diaEscolhido = "do Sandálio"
+//             break;
+//         case "18":
+//             diaEscolhido = "do Chupeto"
+//             break;
+//         case "19":
+//             diaEscolhido = "do Lesmo"
+//             break;
+//         case "20":
+//             diaEscolhido = "do Borboleto"
+//             break;
+//         case "21":
+//             diaEscolhido = "do Monnaliso"
+//             break;
+//         case "22":
+//             diaEscolhido = "do Virilho"
+//             break;
+//         case "23":
+//             diaEscolhido = "do Cervejo"
+//             break;
+//         case "24":
+//             diaEscolhido = "do Gasolino"
+//             break;
+//         case "25":
+//             diaEscolhido = "do Tigreso"
+//             break;
+//         case "27":
+//             diaEscolhido = "do Caniço"
+//             break;
+//         case "28":
+//             diaEscolhido = "do Margarido"
+//             break;
+//         case "29":
+//             diaEscolhido = "do Bariátrico"
+//             break;
+//         case "30":
+//             diaEscolhido = "do Cerejo"
+//             break;
+//         case "31":
+//             diaEscolhido = "do Lacraio"
+//             break;
+//         default:
+//             diaEscolhido;
+//     }
+
+//     alert(`Seu nome é ${mesEscolhido} ${diaEscolhido}`);
+// }
+
+
+
+function cadastroLogin() {
+    let loginEscolinhdo = document.querySelector('#login').value;
+    let senhaEscolhida = document.querySelector('#senha').value;
+
+    let login = "";
+    let senha = "";
+
+    switch (loginEscolinhdo) {
+        case "login1":
+            login = ""
             break;
-        case "2":
-            mesEscolhido = "Seborreio"
-            break;
-        case "3":
-            mesEscolhido = "Sirico"
-            break;
-        case "4":
-            mesEscolhido = "Xoxoto"
-        case "5":
-            mesEscolhido = "Menstruado"
-            break;
-        case "6":
-            mesEscolhido = "Delicio"
-            break;
-        case "7":
-            mesEscolhido = "Caga tronco"
-            break;
-        case "8":
-            mesEscolhido = "Mestruado"
-            break;
-        case "9":
-            mesEscolhido = "Pertubados"
-            break;
-        case "10":
-            mesEscolhido = "Hemorroido"
-            break;
-        case "11":
-            mesEscolhido = "Testosterono"
-            break;
-        case "12":
-            mesEscolhido = "Lapiseiro"
-            break;
+
         default:
-            mesEscolhido;
+            login;
     }
 
 
-    switch (diaE) {
-        case "1":
-            diaEscolhido = "do Tuti fruto"
-            break;
-        case "2":
-            diaEscolhido = "do Tadalafilo"
-            break;
-        case "3":
-            diaEscolhido = "do Prexeco"
-            break;
-        case "4":
-            diaEscolhido = "do Tilápio"
-            break;
-        case "5":
-            diaEscolhido = "do Camisinho"
-            break;
-        case "6":
-            diaEscolhido = "do Macaxeiro"
-            break;
-        case "7":
-            diaEscolhido = "do Jiboio"
-            break;
-        case "8":
-            diaEscolhido = "do Cenouro"
-            break;
-        case "9":
-            diaEscolhido = "do Surubo"
-            break;
-        case "10":
-            diaEscolhido = "do Lagartixo"
-            break;
-        case "11":
-            diaEscolhido = "do Toscano"
-            break;
-        case "12":
-            diaEscolhido = "dO Janjolão"
-            break;
-        case "13":
-            diaEscolhido = "do Pitango"
-            break;
-        case "14":
-            diaEscolhido = "do Vidoco"
-            break;
-        case "15":
-            diaEscolhido = "do Danono"
-            break;
-        case "16":
-            diaEscolhido = "do Bisteco"
-            break;
-        case "17":
-            diaEscolhido = "do Sandálio"
-            break;
-        case "18":
-            diaEscolhido = "do Chupeto"
-            break;
-        case "19":
-            diaEscolhido = "do Lesmo"
-            break;
-        case "20":
-            diaEscolhido = "do Borboleto"
-            break;
-        case "21":
-            diaEscolhido = "do Monnaliso"
-            break;
-        case "22":
-            diaEscolhido = "do Virilho"
-            break;
-        case "23":
-            diaEscolhido = "do Cervejo"
-            break;
-        case "24":
-            diaEscolhido = "do Gasolino"
-            break;
-        case "25":
-            diaEscolhido = "do Tigreso"
-            break;
-        case "27":
-            diaEscolhido = "do Caniço"
-            break;
-        case "28":
-            diaEscolhido = "do Margarido"
-            break;
-        case "29":
-            diaEscolhido = "do Bariátrico"
-            break;
-        case "30":
-            diaEscolhido = "do Cerejo"
-            break;
-        case "31":
-            diaEscolhido = "do Lacraio"
+    switch (senhaEscolhida) {
+        case "senha1":
+            senha = ""
             break;
         default:
-            diaEscolhido;
+            senha;
     }
 
-    alert(`Seu nome é ${mesEscolhido} ${diaEscolhido}`);
+    alert(`Seu Login é: ${loginEscolinhdo} anote sua senha: ${senhaEscolhida}`);
 }
